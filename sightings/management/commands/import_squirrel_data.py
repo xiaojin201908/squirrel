281e24369feaf9b853b873debd26eb3a1ac75ff9
xiaojin201908 <57262342+xiaojin201908@users.noreply.github.com>
2019-12-03 23:29:11 -0500

    Update import_squirrel_data.py

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -18,10 +18,9 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.models.loading import get_model
 
 class Command(BaseCommand):
-        
+    args = 'Model.csv'
     help = 'Import `Model`.csv into `Model` database.'
-    def add_arguments(self, paser):
-        parser.add_argument('path')
+
     def handle(self, *args, **options):
         if len(args) != 1:
             raise CommandError ("Invalid Invocation. See help.")
@@ -30,16 +29,30 @@ class Command(BaseCommand):
         if not os.path.exists (csvPath):
             raise CommandError ("%s doesnt exist." %csvPath)
 
-         model, _ = os.path.splitext (os.path.basename(csvPath))
-         Model = get_model ("lotaya", model.title ())
-         if not Model:
-             raise CommandError ("%s Model doesn't exist'")
+        model, _ = os.path.splitext (os.path.basename(csvPath))
+        Model = get_model ("lotaya", model.title ())
+        if not Model:
+            raise CommandError ("%s Model doesn't exist'")
 
-         model_fields = [f.name for f in Model._meta.fields]
-         fields_name = []
-         with open (csvPath, 'rb') as csvFile:
-             reader = csv.reader (csvFile, delimiter=',', quotechar="\"")
-             fields_name = reader.next ()
+        model_fields = [f.name for f in Model._meta.fields]
+        fields_name = []
+        with open (csvPath, 'rb') as csvFile:
+            reader = csv.reader (csvFile, delimiter=',', quotechar="\"")
+            fields_name = reader.next ()
+            for i, _ in enumerate(fields_name):
+                fields_name[i] = fields_name[i].lower ()
+                fields_name[i] = fields_name[i].replace (' ', '_')
+                if not fields_name[i] in model_fields:
+                    raise CommandError ("%s field doesn't exists in %s Model" %(fields_name[i], Model))
+
+            for row in reader:
+                try:
+                    obj = Model()
+                    for i, field in enumerate(row):
+                        setattr (obj, fields_name[i], field)
+                    obj.save ()
+                except Exception,e:
+                    raise CommandError(e)
                                                                                                                                                                                  for i, _ in enumerate(fields_name):
                                                                                                                                                                                      fields_name[i] = fields_name[i].lower ()
                                                                                                                                                                                      fields_name[i] = fields_name[i].replace (' ', '_')
@@ -50,6 +63,6 @@ class Command(BaseCommand):
                                                                                                                                                                                      try:
                                                                                                                                                                                          obj = Model()
                                                                                                                                                                                          for i, field in enumerate(row):                                                                                                                                                                                                                                                                                                             setattr (obj, fields_name[i], field)
-                     obj.save ()
+                     
                                                                                                                                                                                                                                                                                                                                                          except Exception,e:
                                                                                                                                                                                          raise CommandError(e)
