88dfd1784b2e08e4aad697c708da70d88f7bdd34
Xinyao Wang <xw2675@columbia.edu>
2019-12-04 16:54:33 +0000

    combine

--- sightings/models.py
+++ sightings/models.py
@@ -1,13 +1,6 @@
 from django.db import models
-
-<<<<<<< HEAD
-class squirrel(modesl.Model):
-    name = models.CharField(
-            help_text=_('Latitude'),
-    )
-=======
 from django.utils.translation import gettext as _
-class sighting(models.Model):
+class squirrel(modesl.Model):
     Latitude = models.FloatField(help_text=_('Latitude'),
                                max_length=10,)
     Longitude = models.FloatField(help_text=_('Latitude'),
@@ -16,5 +9,3 @@ class sighting(models.Model):
     def __str__(self):
       return self.Unique_Squirrel_ID
 
-   
->>>>>>> 015401899814cf97bd667cf32b58f984765febb3
