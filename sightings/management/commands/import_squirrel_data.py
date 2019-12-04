1433d077e5d8eccbd4a167ed9fa4b04845132fd0
Xiao Jin <xj2242@columbia.edu>
2019-12-04 04:26:45 +0000

    add a management command file

---
+++ sightings/management/commands/import_squirrel_data.py
@@ -0,0 +1,55 @@
+#from django.core.management.base import BaseCommand,CommandError
+#import pandas as pd
+
+#class Command(BaseCommand):
+   # help ='import squirrel data'
+    #def add_argument(self, parser):
+       # parser.add_argument('path')
+
+    #def handle(self, *args, **kwargs):
+       # path = kwargs['path']
+       # pd.read_csv(path)
+
+
+import os
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.loading import get_model
+
+class Command(BaseCommand):
+        
+    help = 'Import `Model`.csv into `Model` database.'
+    def add_arguments(self, paser):
+        parser.add_argument('path')
+    def handle(self, *args, **options):
+        if len(args) != 1:
+            raise CommandError ("Invalid Invocation. See help.")
+
+        csvPath = args[0]
+        if not os.path.exists (csvPath):
+            raise CommandError ("%s doesnt exist." %csvPath)
+
+         model, _ = os.path.splitext (os.path.basename(csvPath))
+         Model = get_model ("lotaya", model.title ())
+         if not Model:
+             raise CommandError ("%s Model doesn't exist'")
+
+         model_fields = [f.name for f in Model._meta.fields]
+         fields_name = []
+         with open (csvPath, 'rb') as csvFile:
+             reader = csv.reader (csvFile, delimiter=',', quotechar="\"")
+             fields_name = reader.next ()
+                                                                                                                                                                                 for i, _ in enumerate(fields_name):
+                                                                                                                                                                                     fields_name[i] = fields_name[i].lower ()
+                                                                                                                                                                                     fields_name[i] = fields_name[i].replace (' ', '_')
+                                                                                                                                                                                     if not fields_name[i] in model_fields:
+                                                                                                                                                                                         raise CommandError ("%s field doesn't exists in %s Model" %(fields_name[i], Model))
+
+                                                                                                                                                                                 for row in reader:
+                                                                                                                                                                                     try:
+                                                                                                                                                                                         obj = Model()
+                                                                                                                                                                                         for i, field in enumerate(row):                                                                                                                                                                                                                                                                                                             setattr (obj, fields_name[i], field)
+                     obj.save ()
+                                                                                                                                                                                                                                                                                                                                                         except Exception,e:
+                                                                                                                                                                                         raise CommandError(e)
