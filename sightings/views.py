a4e70fbd2adf96133485ac421705b1a613ba8d9f
ellen26 <56944883+ellen26@users.noreply.github.com>
2019-12-05 09:58:34 -0500

    Update views.py

--- sightings/views.py
+++ sightings/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse('Hi!')
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.db.models import Count
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views import generic
+
+from .models import Sighting
+from .forms import SightingForm
